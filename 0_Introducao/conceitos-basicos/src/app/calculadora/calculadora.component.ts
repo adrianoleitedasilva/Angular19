cbca35867901f4a8e43e1db6c2d370f5d4923a9d
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2026-03-05 14:15:57 -0300

    add: add os códigos do CSS e do TS Iniciais

--- 0_Introducao/conceitos-basicos/src/app/calculadora/calculadora.component.ts
+++ 0_Introducao/conceitos-basicos/src/app/calculadora/calculadora.component.ts
@@ -4,8 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-calculadora',
   imports: [],
   templateUrl: './calculadora.component.html',
-  styleUrl: './calculadora.component.scss'
+  styleUrl: './calculadora.component.scss',
 })
 export class CalculadoraComponent {
+  numero1: number = 0; // Poderiamos inserir uma interrogacao logo após o nome da var para tornar a declaracao do valor opcional.
+  numero2: number = 0;
+  resultado: number = 0;
 
+  calculaResultado() {
+    console.log('Chamando o método para calcular o resultado!');
+    console.log('Numero1: ' + this.numero1);
+    console.log('Numero2: ' + this.numero2);
+    this.resultado = this.numero1 + this.numero2;
+  }
 }
