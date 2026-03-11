6444531bc9ee71ae50da740b4519551dbf3affee
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2026-03-11 15:36:19 -0300

    update: atualizando o  lista component ts

--- 1_Introducao/conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
+++ 1_Introducao/conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '../../../node_modules/@angular/common/common_module.d-NEF7UaHr';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-compras.component.html',
-  styleUrl: './lista-compras.component.scss'
+  styleUrl: './lista-compras.component.scss',
 })
 export class ListaComprasComponent {
+  item: string = '';
 
+  adicionarItem() {
+    console.log('Item recebido: ', this.item);
+    this.item = '';
+  }
 }
