52061e9971a4a71f3330a5e354a5e6a4a14fb6fc
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2026-03-11 17:06:32 -0300

    update: da lógica de exibicao da lsita preenchida

--- 1_Introducao/conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
+++ 1_Introducao/conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
@@ -1,18 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgClass } from '../../../node_modules/@angular/common/common_module.d-NEF7UaHr';
+import { ItemLista } from './item-lista';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss',
 })
 export class ListaComprasComponent {
   item: string = '';
+  lista: ItemLista[] = [];
 
   adicionarItem() {
     console.log('Item recebido: ', this.item);
+
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
     this.item = '';
+
+    console.table(this.lista);
   }
 }
